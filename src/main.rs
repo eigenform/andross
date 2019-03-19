bc022e2207c54d18c03c56785df032d06e98ae8b
meta <hosaka-corp@users.noreply.github.com>
2019-03-19 03:08:31 -0500

    prep for actually parsing data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate mio;
 extern crate bus;
 extern crate time;
+extern crate byteorder;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+
+use std::io::Cursor;
+use std::io::SeekFrom;
+use byteorder::{ReadBytesExt, BigEndian, LittleEndian};
 
 use std::io::prelude::*;
 use mio::{Events, Poll, Ready, PollOpt, Token};
@@ -21,6 +31,71 @@ fn timestamp() -> f64 {
     mills
 }
 
+// For more complex synchronization between threads
+const SLEEP: usize          = 0x00;
+const UPDATE: usize         = 0xd0;
+const FLUSH: usize          = 0xd1;
+
+// Slippi commands
+const EVENT_PAYLOADS: u8    = 0x35; 
+const GAME_START: u8        = 0x36; 
+const PRE_FRAME: u8         = 0x37; 
+const POST_FRAME: u8        = 0x38; 
+const GAME_END: u8          = 0x39; 
+
+// Hashmap for Slippi commands. Needs lazy evaluation, and also needs to have
+// a static lifetime so we can reference it wherever/whenever. I assume that,
+// because we need to change this later during runtime, this also means we 
+// need to use a Mutex to serialize writes to this :^( 
+lazy_static! {
+    static ref SLIP_CMD: Mutex<HashMap<u8, u16>> = Mutex::new({
+        let mut m = HashMap::new();
+        m.insert(GAME_START, 0);
+        m.insert(PRE_FRAME, 0);
+        m.insert(POST_FRAME, 0);
+        m.insert(GAME_END, 0);
+        m
+    });
+}
+
+fn parse_message(msg: &Vec<u8>) {
+
+    // Ignore HELO packets
+    if (msg.len() == 5) && (msg == b"HELO\x00") {
+        println!("got HELO");
+        return; 
+    }
+
+    let mut cur = 0;
+    let mut rdr = Cursor::new(msg);
+
+    loop {
+
+        let cmd = rdr.read_u8().unwrap();
+        match cmd {
+            EVENT_PAYLOADS  => {
+                let size = rdr.read_u8().unwrap();
+                let num = (size - 1) / 0x3;
+                for i in 0..num {
+                    let k = rdr.read_u8().unwrap();
+                    let l = rdr.read_u16::<LittleEndian>().unwrap();
+                    SLIP_CMD.lock().unwrap().insert(k, l);
+                    println!("[console]\tFound command {:x}, len 0x{:x}", k, l);
+                }
+            },
+            GAME_START      => {
+                println!("[console]\tDetected GAME_START");
+            },
+            GAME_END        => {
+                println!("[console]\tDetected GAME_END");
+            },
+
+            _               => {
+            },
+        };
+    }
+}
+
 fn main() {
 
     // Set up a poll handle and container for polling events
@@ -29,7 +104,7 @@ fn main() {
 
     // Use the 'bus' crate to broadcast on channels; we need to serialize any
     // access to this because broadcast() and add_rx() require ownership
-    let mut bus: Arc<Mutex<Bus<usize>>> = Arc::new(Mutex::new(Bus::new(1)));
+    let mut bus: Arc<Mutex<Bus<usize>>> = Arc::new(Mutex::new(Bus::new(UPDATE)));
  
     // Set up a global buffer for messages from console
     let global_buf: Arc<Mutex<Vec<Vec<u8>>>> = Arc::new(Mutex::new(vec![vec![]]));
@@ -73,6 +148,11 @@ fn main() {
  * We expect the console to send new data roughly every ~16ms. Everything else 
  * going on needs to fit nicely into this time-window, otherwise we might end
  * up introducing some extra delay during mirroring.
+ *
+ * One interesting problem is: how do we deal managing the global buffer?
+ * Ideally, we flush on GAME_END commands and reset the cursors for consumers.
+ * How do we synchronize this? We technically need to wait for consumers to
+ * finish sending GAME_END to their remote clients.
  */
     let console_bus = bus.clone();
     let console_buf = global_buf.clone();
@@ -81,7 +161,7 @@ fn main() {
 
         println!("[console]\tStarted console socket!");
 
-        'poll_console_sock: loop {
+        'console_loop: loop {
             poll.poll(&mut events, None);
             for event in &events {
 
@@ -92,19 +172,22 @@ fn main() {
                     if event.readiness().is_hup() {
                         console_stream.shutdown(mio::tcp::Shutdown::Both);
                         println!("[console]\tThe console hung up our connection.");
-                        break 'poll_console_sock;
+                        break 'console_loop;
                     }
 
                     // Read the message into a vector
                     let mut message = vec![];
                     console_stream.read_to_end(&mut message);
 
+                    // Parse Slippi commands within the message
+                    //parse_message(&message);
+
                     // Push the vector onto the global buffer
                     console_buf.lock().unwrap().push(message);
 
-                    // Emit a message to all running consumer threads
-                    console_bus.lock().unwrap().broadcast(1);
-                    println!("[console]\t{:?}", timestamp());
+                    // Emit a message to update all running consumer threads
+                    console_bus.lock().unwrap().broadcast(UPDATE);
+                    //println!("[console]\t{:?}", timestamp());
                 } 
             }
         }
@@ -125,11 +208,12 @@ fn main() {
  * clients to fall behind, etc). There's probably some better way of dealing
  * with that via interactions between the read cursor and channel messages.
  */
-    let mut consumer_token = 0;
+    let mut tid = 0;
     let listener = TcpListener::bind("127.0.0.1:666").unwrap();
     for s in listener.incoming() {
-        consumer_token += 1;
+        tid += 1;
 
+        let mut state = SLEEP;
         let mut read_cur = 0;
         let mut rx = bus.lock().unwrap().add_rx();
         let mut stream = TcpStream::from_stream(s.unwrap()).unwrap();
@@ -139,11 +223,21 @@ fn main() {
         stream.set_nodelay(true);
 
         thread::spawn(move || {
-            println!("[token-{}] Thread spawned for consumer", consumer_token);
-            loop {
+            println!("[token-{}] Thread spawned for consumer", tid);
+            'consumer_loop: loop {
 
                 // We wait off-CPU here until we get a channel message
-                rx.recv();
+                println!("{:?}", rx.recv());
+                match rx.recv().unwrap() {
+                    UPDATE      => {
+                        println!("[token-{}]\tUPDATE", tid);
+                        state = UPDATE;
+                    },
+                    _           => {
+                        println!("[token-{}]\tUnknown sync message", tid);
+                        break 'consumer_loop;
+                    },
+                };
 
                 // Block until we acquire the lock and unwrap the buffer
                 let buffer = consumer_buf.lock().unwrap();
@@ -151,8 +245,14 @@ fn main() {
 
                 // Read and send() until we catch up to the write cursor
                 while read_cur < write_cur {
-                    stream.write(buffer.get(read_cur).unwrap());
-                    println!("[token-{}]\t{:?}", consumer_token, timestamp());
+                    match stream.write(buffer.get(read_cur).unwrap()) {
+                        Ok(n) => {},
+                        Err(y) => {
+                            println!("[token-{}]\tDisconnected ({})", tid, y);
+                            break 'consumer_loop;
+                        },
+                    };
+                    //println!("[token-{}]\t{:?}", tid, timestamp());
                     read_cur += 1;
                 }
             }
