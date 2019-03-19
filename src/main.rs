98bee14647436930b7415518df094447b62d78c1
meta <hosaka-corp@users.noreply.github.com>
2019-03-18 23:52:44 -0500

    fix thread synchronization
    - Earlier, 'spmc' was only allowing us to put a single thread on-CPU
      after we retrieved data for a frame. Use the 'bus' crate broadcast
      on a channel to all threads. This causes all consumer threads to
      pick up work during the window in-between frames.
    
    - Add some nicer comments, remove dead code, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate mio;
-extern crate spmc;
+extern crate bus;
+extern crate time;
 
 use std::io::prelude::*;
 use mio::{Events, Poll, Ready, PollOpt, Token};
@@ -10,35 +11,35 @@ use std::net::{TcpListener, SocketAddr};
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc,Mutex};
+use bus::Bus;
 
 const MAIN_THREAD_CYCLE: Duration = Duration::from_millis(10);
 
+fn timestamp() -> f64 {
+    let timespec = time::get_time();
+    let mills: f64 = timespec.sec as f64 + (timespec.nsec as f64 / 1000.0 / 1000.0 / 1000.0);
+    mills
+}
+
 fn main() {
 
-/* Set up some state before actually starting to do any I/O:
- *  - Various mio objects
- *  - Thread synchronization things
- *  - A buffer for threads to share
- */
-
-    // Set up a poll handle and container for events
+    // Set up a poll handle and container for polling events
     let poll = Poll::new().unwrap();
     let mut events = Events::with_capacity(1024);
 
-    // Set up a single-producer, multiple-consumer channel
-    let (tx, rx) = spmc::channel();
+    // Use the 'bus' crate to broadcast on channels; we need to serialize any
+    // access to this because broadcast() and add_rx() require ownership
+    let mut bus: Arc<Mutex<Bus<usize>>> = Arc::new(Mutex::new(Bus::new(1)));
  
     // Set up a global buffer for messages from console
     let global_buf: Arc<Mutex<Vec<Vec<u8>>>> = Arc::new(Mutex::new(vec![vec![]]));
 
 
-/* Set up the console stream and register with Poll. The connect() call is
- * non-blocking with mio, so an immediate HUP indicates that we couldn't 
- * connect to the server (and should just terminate the program).
+/* Set up the console stream and register with Poll. The connect() call here 
+ * is implemented in mio (non-blocking), so an immediate HUP should indicate 
+ * if we failed to connect (and should proceed to just terminate the program).
  */
-
     // Connect to the console stream
-    //let addr: SocketAddr = "127.0.0.1:8000".parse().unwrap();
     let addr: SocketAddr = "10.200.200.5:666".parse().unwrap();
     let mut console_stream = match TcpStream::connect(&addr) {
         Ok(console_stream) => console_stream,
@@ -60,24 +61,26 @@ fn main() {
         }
     }
 
-/* This thread handles the console stream by doing the following:
+
+/* Spawn a thread for handling the console stream, which does the following:
  *
  *  - If a socket is readable:
  *      - Check if the console HUP'ed; die if this is the case
- *      - Create a Vec, read data from the socket into it
- *      - Acquire lock, push Vec onto the global buffer
- *      - Emit a message on our spmc channel to tell consumer threads
+ *      - Read some amount of data from socket into a vector
+ *      - Acquire lock, push vector onto the global buffer
+ *      - Broadcast on channel, causing all consumer threads to go on-CPU
  *
- *  - If a socket isn't readable:
- *      - Call thread::yield_now() and go off-CPU (maybe this is slow?)
- *
+ * We expect the console to send new data roughly every ~16ms. Everything else 
+ * going on needs to fit nicely into this time-window, otherwise we might end
+ * up introducing some extra delay during mirroring.
  */
-
+    let console_bus = bus.clone();
     let console_buf = global_buf.clone();
+
     thread::spawn(move || {
+
         println!("[console]\tStarted console socket!");
 
-        // Main loop - wait around for messages on the socket
         'poll_console_sock: loop {
             poll.poll(&mut events, None);
             for event in &events {
@@ -91,97 +94,72 @@ fn main() {
                         println!("[console]\tThe console hung up our connection.");
                         break 'poll_console_sock;
                     }
-                    //println!("[console]\tSocket ready to read");
 
-                    // Read a message off the stream and append it to the buffer
+                    // Read the message into a vector
                     let mut message = vec![];
                     console_stream.read_to_end(&mut message);
 
-                    // Push a message onto the buffer; emit message on spmc channel
+                    // Push the vector onto the global buffer
                     console_buf.lock().unwrap().push(message);
-                    tx.send(true);
-                } 
 
-                // If the socket isn't readable, let's go off-CPU for now
-                else {
-                    thread::yield_now();
-                }
+                    // Emit a message to all running consumer threads
+                    console_bus.lock().unwrap().broadcast(1);
+                    println!("[console]\t{:?}", timestamp());
+                } 
             }
         }
     });
 
 
-
-/* This thread handles consumer streams by doing the following:
+/* Start a server, then spawn a consumer thread when we accept() some client.
+ * A consumer thread behaves like so:
  *
- *  - Go off-CPU until we fetch a message on the spmc channel
- *      - Acquire lock, fetch the latest write cursor
+ *  - Go off-CPU until we fetch a message on the channel
+ *      - Acquire lock and take access to the buffer
  *      - While our local read cursor is behind the write cursor:
  *          - Fetch a message and send it to the client
  *          - Increment our local read cursor
- *      - Go off-CPU again (maybe un-necessary?)
  *
+ * Note that it's _probably_ possible for a client to become very far behind,
+ * and somehow take up tons of time in-between frames (which would cause other
+ * clients to fall behind, etc). There's probably some better way of dealing
+ * with that via interactions between the read cursor and channel messages.
  */
-
-
     let mut consumer_token = 0;
     let listener = TcpListener::bind("127.0.0.1:666").unwrap();
     for s in listener.incoming() {
-
-        // Local read cursor
-        let mut read_cur = 0;
-
-        // Token for this thread's socket
         consumer_token += 1;
 
-        // This thread's reference to the global buffer
+        let mut read_cur = 0;
+        let mut rx = bus.lock().unwrap().add_rx();
+        let mut stream = TcpStream::from_stream(s.unwrap()).unwrap();
         let consumer_buf = global_buf.clone();
 
-        // This thread's copy of the receiving end of the spmc channel
-        let rx = rx.clone();
-
-        // Stream associated with this thread
-        let mut stream = TcpStream::from_stream(s.unwrap()).unwrap();
+        // Use TCP_NODELAY - probably required to *go fast*
         stream.set_nodelay(true);
-
-        // Poll/event data associated with this thread
-        let consumer_poll = Poll::new().unwrap();
-        let mut events = Events::with_capacity(1024);
-
-        //consumer_poll.register(&stream, Token(consumer_token), Ready::all(), 
-        //                       PollOpt::edge()).unwrap();
 
         thread::spawn(move || {
             println!("[token-{}] Thread spawned for consumer", consumer_token);
             loop {
 
-                // Wait for an spmc channel message; needed to serialize things.
-                // I think that this puts us off-CPU (which is good!)
+                // We wait off-CPU here until we get a channel message
                 rx.recv();
 
                 // Block until we acquire the lock and unwrap the buffer
                 let buffer = consumer_buf.lock().unwrap();
                 let write_cur = buffer.len();
 
-                // Do reads until we catch up to the write cursor
+                // Read and send() until we catch up to the write cursor
                 while read_cur < write_cur {
-
                     stream.write(buffer.get(read_cur).unwrap());
-
-                    //println!("Got message number {}: {:?}", read_cur, 
-                    //    buffer.get(read_cur).unwrap().iter());
-
-                    // Increment our local read cursor and send data to client
+                    println!("[token-{}]\t{:?}", consumer_token, timestamp());
                     read_cur += 1;
                 }
-
-                // Go off-CPU
-                thread::yield_now();
             }
         });
     }
 
-    // Let the main thread just wait around, or something
+    // Let the main thread just wait around, for now
     loop { thread::sleep(MAIN_THREAD_CYCLE); }
 
 }
